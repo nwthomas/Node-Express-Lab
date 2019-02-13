b7dc94c2aa1859a4b53288ec2b0996b88251f582
Nathan Thomas <nwthomas@me.com>
2019-02-12 16:08:57 -0800

    Create .delete() request functionality on server

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -33,7 +33,6 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  console.log(req.body);
   try {
     const db = await DB.insert(req.body);
     res.status(201).json(db);
@@ -56,6 +55,21 @@ router.put("/:id", async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "Error updating the post."
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const db = await DB.remove(req.params.id);
+    if (db > 0) {
+      res.status(200).json({ message: "The post has been deleted." });
+    } else {
+      res.status(404).json({ message: "The post could not be found." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Error deleting the post."
     });
   }
 });
